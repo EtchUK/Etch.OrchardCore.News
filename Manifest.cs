a0b6a4d20d0c37ab6896947e7cbce0d70faf301c
Peter Keating <peter@moov2.com>
2019-10-17 00:36:31 +0100

    Create module via module boilerplate template

---
+++ Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Author = "Etch UK.",
+    Category = "Content",
+    Description = "Configures content definitions for news.",
+    Name = "News",
+    Version = "0.0.1",
+    Website = "https://etchuk.com"
+)]
---
+++ README.md
@@ -0,0 +1,7 @@
+# Etch.OrchardCore.News
+
+Configures content definitions for news.
+
+## Orchard Core Reference
+
+This module is referencing the RC1 build of Orchard Core ([`1.0.0-rc1-10004`](https://www.nuget.org/packages/OrchardCore.Module.Targets/1.0.0-rc1-10004)).
