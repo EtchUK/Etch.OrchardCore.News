df8d9d292f28269ca5cebf38dfeae394ddecadd1
Peter Keating <pete.keating@etchuk.com>
2021-10-08 10:11:34 +0100

    v1.0.1

--- Manifest.cs
+++ Manifest.cs
@@ -5,7 +5,7 @@ using OrchardCore.Modules.Manifest;
     Category = "Content",
     Description = "Configures content definitions for news.",
     Name = "News",
-    Version = "1.0.0",
+    Version = "1.0.1",
     Website = "https://etchuk.com",
     Dependencies = new string[] {
         "Etch.OrchardCore.Blocks",
