d134f88cf9c9fb254451bc87aa7dfbf425372f1c
Peter Keating <pete.keating@etchuk.com>
2022-03-31 17:18:51 +0100

    Include tags taxonomy field in index

--- Migrations.cs
+++ Migrations.cs
@@ -1,4 +1,6 @@
-﻿using OrchardCore.Data.Migration;
+﻿using OrchardCore.ContentManagement.Metadata;
+using OrchardCore.Data.Migration;
+using OrchardCore.Indexing;
 using OrchardCore.Recipes.Services;
 using System.Threading.Tasks;
 
@@ -6,10 +8,12 @@ namespace Etch.OrchardCore.News
 {
     public class Migrations : DataMigration
     {
+        private readonly IContentDefinitionManager _contentDefinitionManager;
         private readonly IRecipeMigrator _recipeMigrator;
 
-        public Migrations(IRecipeMigrator recipeMigrator)
+        public Migrations(IContentDefinitionManager contentDefinitionManager, IRecipeMigrator recipeMigrator)
         {
+            _contentDefinitionManager = contentDefinitionManager;
             _recipeMigrator = recipeMigrator;
         }
 
@@ -18,6 +22,19 @@ namespace Etch.OrchardCore.News
             await _recipeMigrator.ExecuteAsync("create.recipe.json", this);
 
             return 1;
+        }
+
+        public int UpdateFrom1()
+        {
+            _contentDefinitionManager.AlterPartDefinition("NewsPost", builder => builder
+                .WithField("Tags", field => field
+                    .WithSettings(new ContentIndexSettings
+                    {
+                        Included = true,
+                        Stored = true
+                    })));
+
+            return 2;
         }
     }
 }
