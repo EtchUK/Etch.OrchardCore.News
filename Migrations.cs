c886217aeacd8c6f1f7b7a53946a29c792b94832
Mehdi Miah <Mehdi.Miah@etchuk.com>
2023-01-18 12:29:58 +0000

    Create new query for tagged post count

--- Migrations.cs
+++ Migrations.cs
@@ -1,7 +1,12 @@
 ﻿using OrchardCore.ContentManagement.Metadata;
 using OrchardCore.Data.Migration;
 using OrchardCore.Indexing;
+using OrchardCore.Queries;
 using OrchardCore.Recipes.Services;
+using OrchardCore.Search.Elasticsearch.Core.Models;
+using OrchardCore.Search.Lucene;
+using OrchardCore.Search.Lucene.Model;
+using System.ComponentModel;
 using System.Threading.Tasks;
 
 namespace Etch.OrchardCore.News
@@ -10,11 +15,13 @@ namespace Etch.OrchardCore.News
     {
         private readonly IContentDefinitionManager _contentDefinitionManager;
         private readonly IRecipeMigrator _recipeMigrator;
+        private readonly IQueryManager _queryManager;
 
-        public Migrations(IContentDefinitionManager contentDefinitionManager, IRecipeMigrator recipeMigrator)
+        public Migrations(IContentDefinitionManager contentDefinitionManager, IRecipeMigrator recipeMigrator, IQueryManager queryManager)
         {
             _contentDefinitionManager = contentDefinitionManager;
             _recipeMigrator = recipeMigrator;
+            _queryManager = queryManager;
         }
 
         public async Task<int> CreateAsync()
@@ -35,6 +42,19 @@ namespace Etch.OrchardCore.News
                     })));
 
             return 2;
+        }
+
+        public async Task<int> UpdateFrom2()
+        {
+            await _queryManager.SaveQueryAsync("ContentByTags", new LuceneQuery
+            {
+                Index = "News",
+                ReturnContentItems = true,
+                Name = "ContentByTags",
+                Template = "{\r\n  \"query\": {\r\n    \"bool\": { \r\n      \"must\": [\r\n        { \"term\": { \"NewsPost.Tags.Ids\": \"{{tagId}}\" } }\r\n      ]\r\n    }\r\n  }\r\n}"
+            });
+
+            return 3;
         }
     }
 }
